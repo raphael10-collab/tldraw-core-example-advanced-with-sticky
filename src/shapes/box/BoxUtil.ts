04287d395edd7db914ad711289fb2c0b7c5e9610
Steve Ruiz <steveruizok@gmail.com>
2022-11-13 13:25:03 +0000

    fix bugs

--- src/shapes/box/BoxUtil.ts
+++ src/shapes/box/BoxUtil.ts
@@ -10,64 +10,69 @@ type T = BoxShape
 type E = SVGSVGElement
 
 export class BoxUtil extends CustomShapeUtil<T, E> {
-  Component = BoxComponent
+	Component = BoxComponent
 
-  Indicator = BoxIndicator
+	Indicator = BoxIndicator
 
-  hideResizeHandles = false
+	hideResizeHandles = false
 
-  getBounds = (shape: T) => {
-    const bounds = Utils.getFromCache(this.boundsCache, shape, () => {
-      const [width, height] = shape.size
+	getBounds = (shape: T) => {
+		const bounds = Utils.getFromCache(this.boundsCache, shape, () => {
+			const [width, height] = shape.size
 
-      return {
-        minX: 0,
-        maxX: width,
-        minY: 0,
-        maxY: height,
-        width,
-        height,
-      } as TLBounds
-    })
+			return {
+				minX: 0,
+				maxX: width,
+				minY: 0,
+				maxY: height,
+				width,
+				height,
+			} as TLBounds
+		})
 
-    return Utils.translateBounds(bounds, shape.point)
-  }
+		return Utils.translateBounds(bounds, shape.point)
+	}
 
-  /* ----------------- Custom Methods ----------------- */
+	/* ----------------- Custom Methods ----------------- */
 
-  canBind = true
+	canBind = true
 
-  getShape = (props: Partial<T>): T => {
-    return {
-      id: nanoid(),
-      type: 'box',
-      name: 'Box',
-      parentId: 'page1',
-      point: [0, 0],
-      size: [100, 100],
-      childIndex: 1,
-      ...props,
-    }
-  }
+	getShape = (props: Partial<T>): T => {
+		return {
+			id: nanoid(),
+			type: 'box',
+			name: 'Box',
+			parentId: 'page1',
+			point: [0, 0],
+			size: [100, 100],
+			childIndex: 1,
+			...props,
+		}
+	}
 
-  shouldRender = (prev: T, next: T) => {
-    return next.size !== prev.size
-  }
+	shouldRender = (prev: T, next: T) => {
+		return next.size !== prev.size
+	}
 
-  getCenter = (shape: T) => {
-    return Utils.getBoundsCenter(this.getBounds(shape))
-  }
+	getCenter = (shape: T) => {
+		return Utils.getBoundsCenter(this.getBounds(shape))
+	}
 
-  hitTestPoint = (shape: T, point: number[]) => {
-    return Utils.pointInBounds(point, this.getBounds(shape))
-  }
+	hitTestPoint = (shape: T, point: number[]) => {
+		return Utils.pointInBounds(point, this.getBounds(shape))
+	}
 
-  hitTestLineSegment = (shape: T, A: number[], B: number[]) => {
-    return intersectLineSegmentBounds(A, B, this.getBounds(shape)).length > 0
-  }
+	hitTestLineSegment = (shape: T, A: number[], B: number[]) => {
+		return intersectLineSegmentBounds(A, B, this.getBounds(shape)).length > 0
+	}
 
-  transform = (shape: T, bounds: TLBounds, initialShape: T, scale: number[]) => {
-    shape.point = [bounds.minX, bounds.minY]
-    shape.size = [bounds.width, bounds.height]
-  }
+	transform = (
+		shape: T,
+		bounds: TLBounds,
+		initialShape: T,
+		scale: number[]
+	) => {
+		shape.point = [bounds.minX, bounds.minY]
+		shape.size = [bounds.width, bounds.height]
+	}
 }
--- src/shapes/sticky/StickyShape.ts
+++ src/shapes/sticky/StickyShape.ts
@@ -3,4 +3,5 @@ import type { TLShape } from '@tldraw/core'
 export interface StickyShape extends TLShape {
   type: 'sticky'
   size: number[]
+  text: string
 }
